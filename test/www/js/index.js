eebe828f954f21efedaa5d91d850c612daa8a9c2
sntahmaz <sntahmaz@gmail.com>
2018-06-29 18:06:36 +0300

    inputs corrected

--- test/www/js/index.js
+++ test/www/js/index.js
@@ -44,10 +44,7 @@ var app = {
             subjectToGdpr = "0";
         }
         
-        var inputs = { : " < ADMOST_ANDROID_APP_ID > ","applicationIdIOS" : " < ADMOST_IOS_APP_ID > ", "userConsent" : " < userConsent >", "subjectToGdpr" : " < subjectToGdpr > "};
-        
-
-        
+     
         var inputs = {"applicationIdAndroid" : "6cc8e89a-b52a-4e9a-bb8c-579f7ec538fe", "applicationIdIOS" : "15066ddc-9c18-492c-8185-bea7e4c7f88c" , "interstitialIdAndroid": "f99e409b-f9ab-4a2e-aa9a-4d143e6809ae", "bannerIdIOS" : "b4009772-de04-42c4-bbaa-c18da9e4a1ab" , "interstitialIdIOS" : "39f74377-5682-436a-9338-9d1c4df410bd", "videoIdIOS" : "2bdefd44-5269-4cbc-b93a-373b74a2f067" ,"bannerIdAndroid" : "86644357-21d0-45a4-906a-37262461df65", "videoIdAndroid" : "88cfcfd0-2f8c-4aba-9f36-cc0ac99ab140", "userConsent" : consent, "subjectToGdpr" : subjectToGdpr};
         
         
